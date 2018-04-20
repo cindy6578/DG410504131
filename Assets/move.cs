cec5c5dd91fc3f253cff5f1188a66a59f6227693
s1050413 <s1050413@gm.pu.eud.tw>
2018-04-20 11:33:54 +0800

    init 5

---
+++ Assets/move.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class move : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+
+   
+    
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float z = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Vertical");
+        //transform.Translate(x, 0, z);
+
+        rb.AddForce(new Vector3(x, 0, z) * speed);
+
+
+
+       
+
+
+    }
+
+   
+    
+
+}
